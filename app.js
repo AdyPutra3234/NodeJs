61c77f842c856ef1760293335ffffecc0c4429f5
AdyPutra3234 <romadona.xtl4@gmail.com>
2020-11-21 20:02:01 +0700

    edit file app.js & install ejs

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     res.render('hello.ejs');
 });
@@ -9,4 +11,6 @@ app.get('/top', (req, res) => {
     res.render('top.ejs');
 });
 
-app.listen(3000);
+app.listen(3234 , () => {
+    console.log('Server started');
+});
