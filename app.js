5346344753fd20412dfe3a7af6454330c546182a
AdyPutra3234 <romadona.xtl4@gmail.com>
2020-11-22 18:16:55 +0700

    install mysql dan penerapan database di app.js

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 
 app.use(express.static('public'));
-
 app.set('view engine', 'ejs');
 
-// app.get('/', (req, res) => {
-//     res.render('hello.ejs');
-// });
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'Adi',
+    password: '@mypass3234',
+    database: 'list_belanja'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('Connected');
+
+    const tableItems = 'CREATE TABLE IF NOT EXISTS items (id INT NOT NULL , name VARCHAR (10) NOT NULL, PRIMARY KEY (id))';
+
+    connection.query(tableItems, (error, result) => {
+        if (error) console.log('Failed to create table items');
+        else console.log('Table Items is ready');
+    });
+})
 
 app.get('/', (req, res) => {
     res.render('top.ejs');
 });
 
 app.get('/index', (req, res) => {
-    res.render('index.ejs');
+    connection.query('SELECT * FROM items', (error, result) => {
+        if (error) console.error(error);
+        res.render('index.ejs', {items: result});
+    });
 });
 
 app.listen(3234 , () => {
